575fcebf74eeb40af7bb052143ffe0c57d36b61c
Your Name <your.email@example.com>
2025-05-23 17:51:49 +0800

    feat: 重新上传项目

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,34 +1,8 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <h1>Vue Rabbit</h1>
-    </header>
-    <main class="main">
-      <h2>我是首页</h2>
-    </main>
+  <div>
+    我是首页
+    <!-- 二级路由出口 -->
+    <RouterView/>
   </div>
 </template>
-
-<script setup>
-</script>
-
-<style scoped>
-.layout {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-.header {
-  background-color: #409eff;
-  color: white;
-  padding: 1rem;
-  text-align: center;
-}
-
-.main {
-  flex: 1;
-  padding: 2rem;
-}
-</style>
 
