4cafec9128df6b60735e520875a979cd88b179ed
Your Name <your.email@example.com>
2025-05-23 17:07:15 +0800

    fix: 修复 Element Plus 样式导入问题

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import './assets/main.css'
+import './styles/element/index.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
+
+import { getCategory } from './apis/textAPI'
+
+getCategory().then(res=>{
+  console.log(res);
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
