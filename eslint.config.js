d3ab6d4a31c791e3eeb359c52f4610692fb4540b
Your Name <your.email@example.com>
2025-05-23 17:26:48 +0800

    chore: 禁用组件命名规则

--- eslint.config.js
+++ eslint.config.js
@@ -21,4 +21,11 @@ export default defineConfig([
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/component-name-in-template-casing': 'off',
+      'vue/component-definition-name-casing': 'off'
+    }
+  }
 ])
